50b34f044b044709d8deabfd5abecfdb26203738
Anas <anasalwardtech@gmail.com>
2026-01-25 01:59:20 +0300

    fix: update CommentSerializer and ReviewSerializer formatting for consistency

--- backend/apps/reviews/serializers.py
+++ backend/apps/reviews/serializers.py
@@ -1,21 +1,35 @@
 from .models import Review, Comment
 from rest_framework import serializers
 from apps.patients.serializers import PatientSerializer
+from apps.users.serializers import UserSerializer
+
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False, read_only=True)
+
     class Meta:
         model = Comment
-        fields = ['id', 'review', 'type', 'user', 'content', 'created_at', 'updated_at']
-        read_only_fields = ['user', 'type', "review",  'created_at', 'updated_at']
+        fields = ["id", "review", "type", "user", "content", "created_at", "updated_at"]
+        read_only_fields = ["user", "type", "review", "created_at", "updated_at"]
 
 
 class ReviewSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
     patient = PatientSerializer(many=False, read_only=True)
+
     class Meta:
         model = Review
-        fields = ['id', 'patient', 'rating', 'content', 'doctor', 'comments', 'created_at', 'updated_at']
-        read_only_fields = ['patient', 'created_at', 'updated_at']
+        fields = [
+            "id",
+            "patient",
+            "rating",
+            "content",
+            "doctor",
+            "comments",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["patient", "created_at", "updated_at"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
